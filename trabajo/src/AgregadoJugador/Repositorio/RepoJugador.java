4264ed844d84922bd266225a794288465fc3192d
Rubén Martín <martinandraderuben@gmail.com>
2025-11-15 11:29:44 +0100

    Arrglado problema con los repos y con la herencia

--- trabajo/src/AgregadoJugador/Repositorio/RepoJugador.java
+++ trabajo/src/AgregadoJugador/Repositorio/RepoJugador.java
@@ -1,12 +1,10 @@
 package AgregadoJugador.Repositorio;
 
-import AgregadoJugador.DireccionJuego;
 import AgregadoJugador.Jugador;
 import Interfaces.IRepositorioExtend;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,111 +15,219 @@ import java.util.Optional;
 
 public class RepoJugador implements IRepositorioExtend<Jugador, Integer> {
 
-    private final File archivo = new File("Jugadores.json");
-    private final ObjectMapper oM = new ObjectMapper();
-    private final ObjectWriter writer = oM.writerWithDefaultPrettyPrinter();
-    private static int contadorID;
-    private Map<Integer, Jugador> listaJugadores;
+    private final File archivo = new File("Jugadores.json");// Fichero donde se guardan los jugadores
+    private final ObjectMapper oM = new ObjectMapper(); // Objeto que permite parsear los datos a json y de json a java
+    private static int contadorID;// Contador autoincremental para los jugadores
+    private Map<Integer, Jugador> listaJugadores; // Clave id de jugadores y el valor el jugador
 
+    /**
+     * Constructor que saca los datos del fichero
+     *
+     * @throws IOException Lanza una excepción en caso de no haber encontrado el fichero
+     */
     public RepoJugador() throws IOException {
         recibirDatosFichero();
     }
 
-    private void comprobarExistenciaClave(Integer o) throws IOException {
-        if (!existsById(o))
-            throw new IllegalArgumentException("En la lista no existe ningún jugador con este id");
-    }
-
-    public List<Jugador> buscarJugadorPorDireccion(DireccionJuego direccionJuego) {
-        return listaJugadores.values().stream().filter(jugador -> jugador.getDireccionJuego().equals(direccionJuego)).toList();
-    }
-
+    /**
+     * Busca en la lista de jugadores por el id introducido
+     *
+     * @param id Id introducido por el usuario
+     * @return En caso de existir el jugador devuelve ese jugador, en caso contrario Optional.empty()
+     */
     @Override
     public Optional<Jugador> findByIdOptional(Integer id) {
         return Optional.ofNullable(listaJugadores.get(id));
     }
 
+    /**
+     * Devuelve todos los jugadores en una lista
+     *
+     * @return Devuelve los jugadores en un objeto tipo List
+     */
     @Override
     public List<Jugador> findAllToList() {
         return List.copyOf(listaJugadores.values());
     }
 
+    /**
+     * Cuenta la cantidad de jugadores presentes en la lista
+     *
+     * @return Devuelve la cantidad de jugadores en la lista
+     */
     @Override
     public long count() {
         return listaJugadores.size();
     }
 
+    /**
+     * Eliminina un jugdor
+     *
+     * @param id Id del jugador a eliminar
+     * @throws IOException lanza una excepción si falla al leer el archivo
+     */
     @Override
     public void deleteById(Integer id) throws IOException {
+        //lee el archivo
         recibirDatosFichero();
+        // Se asegura de que el id si exista en el archivo
         comprobarExistenciaClave(id);
+        // En caso de si existir lo elimina
         listaJugadores.remove(id);
+        // Y escribe esos datos en el fichero
         escribirDatos();
     }
 
+    /**
+     * Elimina todos los datos del fichero
+     *
+     * @throws IOException Lanza la excepción cuando no se puede leer correctamente el archivoi
+     */
     @Override
     public void deleteAll() throws IOException {
-        contadorID = 0;
+        // Primero vacía la lista de jugadores
         listaJugadores = new HashMap<>();
+        // Luego escribe esa lista vacía en el fichero
         escribirDatos();
     }
 
+    /**
+     * Comprueba si existe un jugador con el id pasado como parámetro
+     *
+     * @param id Id del jugador a buscar
+     * @return Devuelve true en caso de que si exista y false en caso contrario
+     */
     @Override
     public boolean existsById(Integer id) {
         return listaJugadores.containsKey(id);
     }
 
+    /**
+     * Busca un jugador por su id
+     *
+     * @param id Id del jugador a buscar
+     * @return Devuelve en caso de encontrarlo el jugador buscado
+     */
     @Override
-    public Jugador findById(Integer id) throws IOException {
+    public Jugador findById(Integer id) {
         comprobarExistenciaClave(id);
         return listaJugadores.get(id);
     }
 
+    /**
+     * Devuelve una colección con todos los jugadores
+     *
+     * @return Devuelve una colección con todos los jugadores
+     */
     @Override
     public Iterable<Jugador> findAll() {
         return listaJugadores.values();
     }
 
+    /**
+     * Guarda el jugador en el fichero
+     *
+     * @param entity Entidad que se va a guardar
+     * @param <S>    Objeto que extiende de jugador
+     * @return Devuele la entidad guardada
+     * @throws IllegalArgumentException Lanza esta excepción en caso de que la lista ya contenga a ese jugador
+     * @throws IOException              Lanza este error cuando la lectura del archivo falla
+     */
     @Override
-    public <S extends Jugador> S save(S entity) throws Exception {
-        if (!(entity instanceof Jugador jugador))
-            throw new IllegalArgumentException("El tipo de dato debe ser un Jugador");
-
+    public <S extends Jugador> S save(S entity) throws IllegalArgumentException, IOException {
+        // Actualizamos los datos del fichero
         recibirDatosFichero();
-        if (listaJugadores.containsValue(jugador))
-            throw new IllegalArgumentException("El jugador ya existe en el archivo");
+        // Si la lista de jugadores contiene al jugador pasado como parámetro
+        if (listaJugadores.containsValue(entity))
+            // Se lanza una excepción que indica que este jugador ya existe
+            throw new IllegalArgumentException("El jugador " + entity.getNombre() + " ya existe en el archivo");
 
-        jugador.setID_JUGADOR(contadorID);
-        listaJugadores.put(jugador.getID_JUGADOR(), jugador);
+        // En caso de que no exista se le pone un id
+        entity.setID_JUGADOR(contadorID);
+        // Se añade el jugador a la lista
+        listaJugadores.put(entity.getID_JUGADOR(), entity);
+        // Y se sobreescribe la lista en el fichero
         escribirDatos();
+        // Devolvemos la entidad guardada
         return entity;
     }
 
+    /**
+     * Actualiza los datos de un jugador
+     *
+     * @param entity Jugador a actualizar
+     * @param <S>    Jugador o clases hijas
+     * @return Devuelve el jugador actualizado
+     * @throws IOException Lanza excepción en caso de que la escritura falle
+     */
+    public void actualizar() throws IOException {
+        recibirDatosFichero();
+        // Escribimos los datos
+        escribirDatos();
+    }
 
+
+    /**
+     * Escribe los datos en el archivo
+     *
+     * @throws IOException Lanza excepción en caso de que la escritura falle
+     */
     private void escribirDatos() throws IOException {
+        // Creamos un map con Json node
         Map<Integer, JsonNode> jsonMap = new HashMap<>();
+        // Recorremos toda la lista de jugadores
         for (Map.Entry<Integer, Jugador> entry : listaJugadores.entrySet()) {
-            // aquí usamos ObjectMapper directamente
+            // Y vamos añadiendo cada posición al Json Map, esto fuerza a que se apliquen las anotaciones
             jsonMap.put(entry.getKey(), oM.valueToTree(entry.getValue()));
         }
 
-        // Guardar JSON en archivo con pretty print
+        // Guardar JSON en archivo con pretty print,esto hace que el archivo sea más bonito y legible
         oM.writerWithDefaultPrettyPrinter().writeValue(archivo, jsonMap);
     }
 
 
+    /**
+     * Recibe los datos del archivo
+     *
+     * @throws IOException Lanza excepción en caso de que la lectura del archivo falle
+     */
     private void recibirDatosFichero() throws IOException {
+        // Si el archivo existe y su longitud es mayor que cero
         if (archivo.exists() && archivo.length() > 0) {
+            // Sacamos del archivo la lista de jugadores
             listaJugadores = oM.readValue(archivo, new TypeReference<Map<Integer, Jugador>>() {
             });
             /*
-             Saca todos los ids del Hash Map y los compara en caso de que la lista este vacía se pondrá por defecto 1
+             Saca todos los ids del Hash Map y los compara, sacando el mayor
+             en caso de que la lista este vacía se pondrá por defecto 1,
              en caso contrario se pondrá último id + 1
              */
             contadorID = listaJugadores.keySet().stream().max(Integer::compareTo).orElse(0) + 1;
         } else {
+            // En caso contrario inicializamos la lista vacía
             listaJugadores = new HashMap<>();
         }
+    }
+
+    /**
+     * Se asegura de que la clave pasada exista dentro del fichero
+     *
+     * @param id a comprobar
+     */
+    private void comprobarExistenciaClave(Integer id) throws IllegalArgumentException {
+        if (!existsById(id))
+            throw new IllegalArgumentException("En la lista no existe ningún jugador con id " + id);
+    }
+
+    /**
+     * Método añadido que se encarga de buscar jugadores por su calle
+     *
+     * @param calle Calle introducida por el usuario para buscar todos los jugadores que tengan esa calle como dirección de juego
+     * @return Devuelve una lista con todos los jugadores que viven en la calle seleccionada
+     */
+    public List<Jugador> buscarJugadorPorDireccion(String calle) {
+        // Filtramos la lista de jugadores por aquellos que vivan en la calle pasada como parámetro
+        return listaJugadores.values().stream().filter(jugador -> jugador.getDireccionJuego().getCalle().equalsIgnoreCase(calle)).toList();
     }
 
 }
---
+++ trabajo/src/AgregadoJugador/mainJugador.java
@@ -0,0 +1,265 @@
+package AgregadoJugador;
+
+import AgregadoJugador.Repositorio.RepoJugador;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class mainJugador {
+    static Scanner teclado = new Scanner(System.in);
+    static List<Jugador> jugs = new ArrayList<>();
+
+    public static void main(String[] args) {
+        jugs.add(new Jugador("02790162D", "Rubén", new DireccionJuego("Madrid", "Calle del besito 32", "1C", "28017")));
+        try {
+            RepoJugador repoJugador = new RepoJugador();
+            menuPrincipal(repoJugador);
+        } catch (IOException e) {
+            System.err.println("Fallo a la hora de leer el archivo " + e.getMessage());
+        }
+
+    }
+
+    private static void menuPrincipal(RepoJugador repoJugador) {
+        int opcionJugador = 0;
+        while (opcionJugador != 13) {
+            try {
+                System.out.println("-------------------------------------------------------");
+                System.out.println("Menú, para salir introduce 12");
+                System.out.println("-------------------------------------------------------");
+                System.out.println("1.Crear Jugador");
+                System.out.println("2.Buscar jugador por id opcional");
+                System.out.println("3.Sacar jugadores a lista");
+                System.out.println("4.Contar jugadores");
+                System.out.println("5.Eliminar jugador");
+                System.out.println("6.Eliminar todos los jugadores");
+                System.out.println("7.Comprobar si un jugador existe");
+                System.out.println("8.Buscar jugador por id");
+                System.out.println("9.Sacar todos los jugadores a un Iterable");
+                System.out.println("10.Guardar jugador");
+                System.out.println("11.Buscar jugadores por calle");
+                System.out.println("12.Actualizar un jugador");
+                System.out.println("-------------------------------------------------------");
+
+                opcionJugador = teclado.nextInt();
+                opcionesPrincipales(opcionJugador, repoJugador);
+
+            } catch (InputMismatchException e) {
+                System.err.println("Introduce solo números del 1-11 " + e.getMessage());
+                teclado.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            } catch (IOException e) {
+                System.err.println("Error al acceder al archivo");
+            }
+
+        }
+    }
+
+    private static void opcionesPrincipales(int opcionJugador, RepoJugador repoJugador) throws IllegalArgumentException, IOException {
+        switch (opcionJugador) {
+            case 1:
+                menuCreacionJugador();
+                break;
+            case 2:
+                buscarPorIDOpcional(repoJugador);
+                break;
+            case 3:
+                System.out.println(repoJugador.findAllToList());
+                break;
+            case 4:
+                System.out.println("La cantidad de jugadores es: " + repoJugador.count());
+                break;
+            case 5:
+                eliminarJugadorPorId(repoJugador);
+                break;
+            case 6:
+                eliminarTodosLosJugadores(repoJugador);
+                break;
+            case 7:
+                comprobarSiJugadorExiste(repoJugador);
+                break;
+            case 8:
+                buscarJugadorPorID(repoJugador);
+                break;
+            case 9:
+                sacarJugadoresIterable(repoJugador);
+                break;
+            case 10:
+                guardarJugador(repoJugador);
+                break;
+            case 11:
+                buscarJugadoresPorCalle(repoJugador);
+                break;
+            case 12:
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    private static void menuCreacionJugador() {
+        int tipoJugador = 0;
+        while (tipoJugador != 3) {
+            try {
+
+                System.out.println("Que tipo de jugador quieres crear? Para salir introduce 3");
+                System.out.println("1.Jugador Normal");
+                System.out.println("2.Jugador Director de juego");
+                tipoJugador = teclado.nextInt();
+
+                switch (tipoJugador) {
+                    case 1:
+                        DireccionJuego direccionJuego1 = crearDireccionJuego();
+                        crearJugador(direccionJuego1);
+                        break;
+                    case 2:
+                        DireccionJuego direccionJuego2 = crearDireccionJuego();
+                        creaDirectorJuego(direccionJuego2);
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("Introduce solo números " + e.getMessage());
+                teclado.nextLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static DireccionJuego crearDireccionJuego() {
+        String ciudad, calle, piso, codigoPostal;
+        DireccionJuego direccionDeJuego;
+        System.out.println("Primero vamos a crear la dirección de juego: ");
+        while (true) {
+            try {
+                teclado.nextLine();
+                System.out.println("Introduce la ciudad:");
+                ciudad = teclado.nextLine();
+                System.out.println("Introduce la calle:");
+                calle = teclado.nextLine();
+                System.out.println("Introduce el piso:");
+                piso = teclado.next();
+                System.out.println("Introduce el código postal");
+                codigoPostal = teclado.next();
+                direccionDeJuego = new DireccionJuego(ciudad, calle, piso, codigoPostal);
+                return direccionDeJuego;
+
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void crearJugador(DireccionJuego direccionJuego) {
+        String dni, nombre;
+        teclado.nextLine();
+        System.out.println("Introduce el nombre del jugador:");
+        nombre = teclado.nextLine();
+        System.out.println("Introduce el DNI del jugador:");
+        dni = teclado.next();
+        jugs.add(new Jugador(dni, nombre, direccionJuego));
+    }
+
+    private static void creaDirectorJuego(DireccionJuego direccionJuego) throws IllegalArgumentException {
+        String dni, nombre;
+        System.out.println("Vamos a crear un jugador");
+
+        System.out.println("Introduce el nombre del jugador:");
+        nombre = teclado.nextLine();
+        System.out.println("Introduce el DNI del jugador:");
+        dni = teclado.next();
+        jugs.add(new DirectorDeJuego(dni, nombre, direccionJuego));
+
+    }
+
+    private static void buscarPorIDOpcional(RepoJugador repoJugador) throws IllegalArgumentException {
+        int idJugador;
+        System.out.println("Introduce el id del jugador que quieras buscar:");
+        idJugador = teclado.nextInt();
+        System.out.println(repoJugador.findByIdOptional(idJugador));
+
+    }
+
+    private static void eliminarJugadorPorId(RepoJugador repoJugador) throws IOException, IllegalArgumentException {
+        int idJugador;
+        System.out.println("Introduce el id del jugador que quieras eliminar:");
+        idJugador = teclado.nextInt();
+        repoJugador.deleteById(idJugador);
+        System.out.println("El jugador ha sido eliminado con éxito");
+    }
+
+    private static void eliminarTodosLosJugadores(RepoJugador repoJugador) throws IOException {
+        int opcion;
+        System.out.println("Estas seguro de que quieres eliminar todos los jugadores, no hay marcha atrás");
+        System.out.println("1.Sí");
+        System.out.println("2.No");
+        opcion = teclado.nextInt();
+        if (opcion == 1)
+            repoJugador.deleteAll();
+    }
+
+    private static void comprobarSiJugadorExiste(RepoJugador repoJugador) {
+        int idJugador;
+        System.out.println("Introduce el id del jugador cuya existencia quieras comprobar");
+        idJugador = teclado.nextInt();
+        if (repoJugador.existsById(idJugador))
+            System.out.println("El jugador con id " + idJugador + " sí existe");
+        else
+            System.out.println("El Jugador con id " + idJugador + " no existe");
+    }
+
+    private static void buscarJugadorPorID(RepoJugador repoJugador) throws IllegalArgumentException {
+        int idJugador;
+        System.out.println("Introduce el id del jugador que quieras buscar");
+        idJugador = teclado.nextInt();
+        System.out.println("Jugador: " + repoJugador.findById(idJugador));
+    }
+
+    private static void sacarJugadoresIterable(RepoJugador repoJugador) {
+        for (Jugador j : repoJugador.findAll()) {
+            System.out.println(j);
+        }
+
+    }
+
+    private static void guardarJugador(RepoJugador repoJugador) throws IOException {
+        if (jugs.isEmpty()) {
+            System.out.println("Aún no has creado ningún jugador");
+            return;
+        }
+        System.out.println("Vamos a intentar guardar todos los jugadores que has creado");
+        for (Jugador jugador : jugs) {
+            try {
+                repoJugador.save(jugador);
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+            }
+
+            System.out.println("Se ha terminado el guardado");
+        }
+    }
+
+    private static void buscarJugadoresPorCalle(RepoJugador repoJugador) {
+        String calle;
+        List<Jugador> jugadores;
+        teclado.nextLine();
+        System.out.println("Introduce la calle de los jugadores que quieras buscar:");
+        calle = teclado.nextLine();
+        jugadores = repoJugador.buscarJugadorPorDireccion(calle);
+        if (jugadores.isEmpty()) {
+            System.out.println("No existen jugadores con esa dirección de juego");
+            return;
+        }
+        for (Jugador jugador : jugadores) {
+            System.out.println(jugador);
+        }
+    }
+
+}
+
