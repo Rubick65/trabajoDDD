f7eb32b298bd1a71316aea29733e9558b89c9283
Hugo de Pablo <hugdpalo@gmail.com>
2026-02-05 13:37:36 +0100

    ytfh

--- trabajo/src/AgregadoPersonaje/ObjetoInventario.java
+++ trabajo/src/AgregadoPersonaje/ObjetoInventario.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 public class ObjetoInventario {
     private String nombre, descripcionObjeto; //Nombre y descripcion del objeto
     private double peso; //Peso del objeto
+    private boolean maldito;
 
     /**
      * Constructor que da valores iniciales a los atributos de clase
@@ -17,6 +18,14 @@ public class ObjetoInventario {
         this.nombre = nombre;
         this.peso = peso;
         this.descripcionObjeto = descripcionObjeto;
+        this.maldito = false;
+    }
+
+    public ObjetoInventario(String nombre, double peso, String descripcionObjeto, boolean maldito) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.descripcionObjeto = descripcionObjeto;
+        this.maldito = maldito;
     }
 
     public ObjetoInventario() {
@@ -36,23 +45,28 @@ public class ObjetoInventario {
         return descripcionObjeto;
     }
 
+    public boolean getMaldito() { return maldito; }
+
     @Override
     public String toString() {
         return "ObjetoInventario{" +
                 "nombre='" + nombre + '\'' +
                 ", descripcionObjeto='" + descripcionObjeto + '\'' +
                 ", peso=" + peso +
+                ", maldito=" + maldito +
                 '}';
     }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         ObjetoInventario that = (ObjetoInventario) o;
-        return Double.compare(peso, that.peso) == 0 && Objects.equals(nombre, that.nombre) && Objects.equals(descripcionObjeto, that.descripcionObjeto);
+        return Double.compare(peso, that.peso) == 0 && maldito == that.maldito && Objects.equals(nombre, that.nombre) && Objects.equals(descripcionObjeto, that.descripcionObjeto);
     }
+
     @Override
     public int hashCode() {
-        return Objects.hash(nombre, descripcionObjeto, peso);
+        return Objects.hash(nombre, descripcionObjeto, peso, maldito);
     }
 }
 
